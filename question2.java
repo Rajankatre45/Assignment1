3c0218071cce987ad55a2f6f2dbc6e785916cb11
Rajankatre45 <115568241+Rajankatre45@users.noreply.github.com>
2022-10-24 10:47:54 +0530

    Add files via upload

---
+++ question2.java
@@ -0,0 +1,24 @@
+package in.ineuron.main;
+
+public class question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n=4;
+		int i,j;
+		
+		for(i=1;i<=n;i++)
+		{
+			for(j=1;j<=n;j++)
+			{
+				System.out.print(i);
+			}
+		System.out.println("");
+		
+		}
+		
+
+	}
+
+}
